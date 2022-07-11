d97abab7cd819581037ebf65195a038420041895
Marius <marius@example.com>
2022-07-11 11:27:12 +0300

    html 5

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,12 @@ console.log('main.ts');
 console.log(items.length);
 
 // 3. sukurti funkcija kuri grazina visu kategoriju masyva.
-const getAllCategoriesArr = (arr: Item[]): string[] => {
-  const allCatsArr: string[] = arr.map((pObj: Item): string => pObj.category);
-  console.log('allCatsArr ===', allCatsArr);
-  return allCatsArr;
+const getSinglePropertyArr = (arr: Item[], property: 'category' | 'image'): string[] => {
+  const singlePropArr: string[] = arr.map((pObj: Item): string => pObj[property]);
+  return singlePropArr;
 };
 
-const allCatsArr: string[] = getAllCategoriesArr(items);
+const allCatsArr: string[] = getSinglePropertyArr(items, 'category');
 
 // 3.1 sukurti funkcija atrinkti is kategoriju tik unikalias reiksmes
 console.log([1, 2, 3].includes(1));
@@ -30,3 +29,7 @@ const getUniquesFromArr = (arr: string[]): string[] => {
 getUniquesFromArr(allCatsArr);
 
 // getUniquesFromArr - reduce
+
+// 5. sukurti funkcija kuri grazina masyva su visais paveiksleliais.
+const allImagesArr = getSinglePropertyArr(items, 'image');
+console.log('allImagesArr ===', allImagesArr);
